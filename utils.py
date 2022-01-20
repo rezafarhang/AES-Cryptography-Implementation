a33e1ef1d6a95037b1b9b6dc7e6a75969ae5489e
amirali <3abd2000@gmail.com>
2022-01-20 18:23:17 +0330

    txt2hex and hex2txt

--- utils.py
+++ utils.py
@@ -2,9 +2,22 @@ from typing import List
 
 
 class TextProcessor:
-    def string_to_hex(self, txt: str) -> str:
-        # 2-1
-        pass
+
+    @staticmethod
+    def string_to_hex(txt: str) -> str:
+        hex_txt = ''
+        for character in txt:
+            hex_txt += hex(ord(character))[-2:]
+        return hex_txt
+
+    @staticmethod
+    def hex_to_string(hex_txt: str) -> str:
+        txt = ''
+        for index in range(0, len(hex_txt), 2):
+            print(index)
+            print(hex_txt[index] + hex_txt[index + 1])
+            txt += chr(int(hex_txt[index] + hex_txt[index + 1], 16))
+        return txt
 
     def hex_to_binary(self, value) -> str:
         # 2-2
